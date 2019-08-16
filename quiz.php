699b7c5a6ce254670f96359cb3b6c9c06b75a957
OTeeEnabor <tobienabor@gmail.com>
2019-08-16 14:22:17 +0200

    The quiz is functional. I have to write code to grade quiz.

--- quiz.php
+++ quiz.php
@@ -15,15 +15,8 @@
 <!--PHP Code starts here -->
 
 <?php
-
-# load the quiz questions and choices into an array of arrays. 
-// function{
-
-// }
 $filename_q = "questions.txt";
 $filename_a = "answers.txt";
-
-#loads a text file which has the questions and choices into an array.
 
 #this returns array of answers if the filename is found in the directory
 function loadAnswer($filename){
@@ -36,34 +29,61 @@ function loadAnswer($filename){
     return $answer_array;
 }
 #this function returns an array of arrays with questions and answers if the file name in the directory. 
-#The is a 2 dimensional array with the first element in the array = question and the second = answer.
+
+#The is a 2 dimensional array with the first element in the array = question and the second element = answer.
 function loadQuestions($filename){
+    $questionsOnly =array();
     if(file_exists($filename)){
         $questionsA = file($filename);
+
+        foreach($questionsA as $key => $value){
+            $questionsOnly[] = explode(":",$value);
+        }
     }
     else{echo"Error file does not exist";}
-    return $questionsA;
+    return $questionsOnly;
 }
 
-#this function will sepererate the array of arrays into an array of questions and array of answers. 
+#create a function that seperates the questions and stores the choices into a seperate arrray.
 
-$QA_array = file($filename_q);
-#load answers text file into an array.
-$answers_array = file($filename_a);
+function showQuestions($questionsA){
+    foreach($questionsA as $key => $value){
+        #show the question
 
-#create an array that has only the questions
-$question_only =array();
+        echo "<br><b>$value[0]</b><br>";
 
-foreach($QA_array as $key => $value){
-    $question_only[]=explode(':',$value);
+        #store choices of each question into a choice array
+         $option = explode(",",$value[1]);
+            foreach($option as $value){
+                $choice_value = substr(trim($value),0,1);
+
+                echo"<input type=\"radio\" name=\"$key\" value=\"$choice_value\">$value</input><br>";
+            }
+    }
+// return $option;
 }
-var_dump($question_only);
-var_dump($QA_array);
-var_dump($answers_array);
 
+## A function to grade the quiz
 
+function gradeQuiz($quizResult){
+    if ($quizResult  < 10){
+        echo "You got $quizResult out of 20 <br>";
+        echo "You can improve your quiz result. Try again.";
+        #Reload the quiz
+    }
+    elseif($quizResult >10 && $quizResult <15){
+        echo "Well done, you got $quizResult out of 20 <br>";
+        echo "Would you like to try again";
+    }
 
+    else{
+        echo "Well done, you got $quizResult out of 20 <br>";  
+    }
 
+}
+
+?>
+<?php
 
 ?>
 <!--PHP code ends here--->
@@ -75,13 +95,54 @@ var_dump($answers_array);
                 CodeSpace General Knowledge Quiz
             </h1>
         </div>
-        <form action="" method="post">
-            input
+<!--START OF FORM HERE-->        
+        <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+<?php
+
+        $loadedQuestions = loadQuestions($filename_q);
+        showQuestions($loadedQuestions);
+?>
+
+        <input type="submit" name="submitquiz" value="Submit Quiz"/>
+
+<!--END OF FORM HERE-->   
+
+<?php
+//Grade the quiz once the submit button is clicked.
+
+if(isset($_POST['submitquiz'])){
+
+    //load the textfile that has the answers. 
+    $answer_array = loadAnswer($filename_a);
+    //Number of answers
+    $answer_count = count($answer_array);
+    //initialize the variable that will count the number of correct answers. 
+    $count_correct = 0;
+
+    //loop through the answer file and compare it with the answers submitted by the quiz taker. 
+
+    foreach($answer_array as $key =>$correct_answer){
+
+        if(isset($_POST[$key])){
+
+            if ($correct_answer == $_POST[$key]){
+
+                $count_correct++;
+            }
+        }
+    }
+
+    echo "<br>Total number of correct answers: $count_correct";
+
+
+
+}
 
 
 
 
-        </form>
+?>
+
         
     </main>
 
