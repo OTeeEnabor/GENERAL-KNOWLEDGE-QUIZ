f9562c6260f1adcd2d6d4abd36f8840ae2e32580
OTeeEnabor <tobienabor@gmail.com>
2019-08-22 19:56:09 +0200

    renamed the first page of quiz as index.php.

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    $filepath = "/CodeSpace/GENERAL-KNOWLEDGE-QUIZ/";
+    ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Caveat+Brush&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One&display=swap" rel="stylesheet">
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!--Custom Stylesheet-->
+    <link href="css/stylesheet.css"rel="stylesheet" type="text/css" >
+
+    <title>CodeSpace General Knowledge Quiz</title>
+</head>
+<body class ="page-body">
+<!--Main Section Starts Here-->
+    <main>
+
+<!--Main Heading Starts Here-->
+        <div class="main-heading">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                    <h1 class="heading-1">CodeSpace General Knowledge Quiz</h1>
+                    <div class="intro-container text-center">
+                        <p id="introPara">
+                            Good day, genius!!!
+                        </p>
+                        <p id ="introPara2">
+                        Here'a little general knowledge quiz that I would like you to attempt:) Make sure you have fun!!   
+                        </p>
+
+                        <a href="quiz.php" class="btn bg-primary launch-button">Are You Up For The Challenge???</a>
+                    </div>
+
+                    </div>
+                </div>
+            
+
+            </div>
+        </div>
+<!--Main Heading Ends Here--> 
+    </main>
+
+
+   <!--Bootstrap JS pluggins--> 
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
